a9f325ec158a583e7f112cd377b5abd461c4c122
Humphrey Joshua <joshuahumphrey579@gmail.com>
2024-12-12 14:05:02 +0100

    updated hub

--- src/app/(users)/the-hub/page.tsx
+++ src/app/(users)/the-hub/page.tsx
@@ -11,9 +11,10 @@ export default async function TheHub(props: Props) {
 	const { contact } = await props.searchParams;
 	return (
 		<div className=" pt-10 md:pt-20">
-			<div className="text-center">
+			<div className="text-center h-[20rem] md:h-[40rem] flex flex-col justify-center items-center gap-3">
 				<h1>Welcome to the Hub</h1>
 				<p>This is the hub for all dinovate-related articles.</p>
+				<p className=" text-5xl text-tertiary">Coming Soon</p>
 			</div>
 			<Footer />
 			<RequestForm open={!!contact} />
